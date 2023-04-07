51f4ff8efba30b067c029413c78f31d73039cb0e
bhaveshraja23 <rampally.b@northeastern.edu>
2023-04-06 20:12:40 -0400

    UI for Dashboard, Menu, Staff, Tables

--- src/pages/admin/tables/index.js
+++ src/pages/admin/tables/index.js
@@ -1,9 +1,37 @@
 import React from "react";
 //styles
-import "./styles.css";
+import "./styles.scss";
 
 const Tables = () => {
-  return <div>Admin Tables</div>;
+  return (
+    <div class="flex-right">
+      <div class="top-bar"></div>
+      <div className="heading">
+        <h3> Tables </h3>
+        <button>
+          
+        </button>
+      </div>
+      <div class="table-container">
+        <div class="table">
+            <h4>Table 1</h4>
+            <p>Occupied</p>
+        </div>
+        <div class="table">
+            <h4>Table 2</h4>
+            <p>Occupied</p>
+        </div>
+        <div class="table">
+            <h4>Table 3</h4>
+            <p>Free</p>
+        </div>
+        <div class="table">
+            <h4>Table 4</h4>
+            <p>Free</p>
+        </div>
+      </div>
+    </div> 
+  );
 };
 
 export default Tables;
