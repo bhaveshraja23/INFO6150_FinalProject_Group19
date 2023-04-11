7164205f662f84bb065de1f412cd2094e998d577
bhaveshraja23 <rampally.b@northeastern.edu>
2023-04-10 20:18:02 -0400

    popup modal's fields, AddMenuItema popup

--- src/pages/admin/menu/index.js
+++ src/pages/admin/menu/index.js
@@ -4,6 +4,7 @@ import "./styles.scss";
 import Button from "react-bootstrap/Button";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AddMenu from "../../../components/popup-models/AddMenu";
+import AddMenuItem from "../../../components/popup-models/AddMenuItem";
 
 const Menu = () => {
   return (
@@ -13,22 +14,42 @@ const Menu = () => {
           <h3> Menu </h3>
           <AddMenu />
       </div>
-      <div class="menu-container">
-      <div class="menu">
+      <div className="menu-container">
+        <div className="menu">
+          <div className="items">
             <h4>Appetizers</h4>
             <p>6 Items</p>
+          </div>
+          <div className="item-button">
+            <AddMenuItem />
+          </div>
         </div>
         <div class="menu">
+          <div className="items">
             <h4>Startes</h4>
             <p>20 Items</p>
+          </div>
+          <div className="item-button">
+            <AddMenuItem />
+          </div>
         </div>
         <div class="menu">
-            <h4>Main Course</h4>
+          <div className="items">
+          <h4>Main Course</h4>
             <p>13 Items</p>
+          </div>
+          <div className="item-button">
+            <AddMenuItem />
+          </div>
         </div>
         <div class="menu">
+          <div className="items">
             <h4>Desserts</h4>
             <p>8 Items</p>
+          </div>
+          <div className="item-button">
+            <AddMenuItem />
+          </div>
         </div>
       </div>
     </div> 
