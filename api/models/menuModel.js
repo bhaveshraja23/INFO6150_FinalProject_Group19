b849feb1509346f162aa14689ddc675d3049f7f5
Saketh <vootla.s@northeastern.edu>
2023-04-14 01:17:37 -0400

    MongoDB Schema Model

---
+++ api/models/menuModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const MenuSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: "required",
+    },
+    description: {
+        type: String,
+        required: "required",
+    },
+    created_at:{
+        type: Date,
+        required: "required",
+        default: Date.now,
+    },
+    updated_at: {
+        type: Date,
+        required: "required",
+        default: Date.now,
+    }
+
+});
+
+const Menu = mongoose.model("Menu", MenuSchema);
+
+module.exports = Menu;
