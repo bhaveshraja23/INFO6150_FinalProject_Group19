14628ec5e7613760829bab2c0942c19a5bb57f57
Saketh <vootla.s@northeastern.edu>
2023-04-14 01:30:30 -0400

    Routing API Endpoints

--- api/routes/router.js
+++ api/routes/router.js
@@ -2,52 +2,59 @@ const express = require('express');
 const router = express.Router();
 
 const controller = require("../controllers/controller");
+const controllerMenuItems = require("../controllers/menuController")
+const controllerOrders = require("../controllers/orderController")
+const controllerTables = require("../controllers/tableController")
 
 module.exports = function (app) {
-
-
     //Sign-in
     router.post("/api/sign-in", controller.loginUser);
     //SignUp
     router.post("/api/signup",controller.createUser);
 
-
-    //Admin
+    // <------------------------ADMIN------------------------------->
     //Dashboard
     //router.post("/api/admin/dashboard",controller.createDashboard);
+
     //Menu
-    router.get("/api/admin/menu", controller.getAllMenu);
-    router.get("/api/admin/menu/:menu_id", controller.getMenuById);
-    router.post("/api/admin/menu", controller.createMenu);
-    router.put("/api/admin/menu/:menu_id", controller.editMenu);
-    router.delete("/api/admin/menu/:menu_id", controller.deleteMenu);
-    router.get("/api/admin/menu/:menu_id/menu-item", controller.getAllMenuItemsByMenuId);
+    router.get("/api/admin/menu", controllerMenuItems.getAllMenu);
+    router.get("/api/admin/menu/:menu_id", controllerMenuItems.getMenuById);
+    router.post("/api/admin/menu", controllerMenuItems.createMenu);
+    router.put("/api/admin/menu/:menu_id", controllerMenuItems.editMenu);
+    router.delete("/api/admin/menu/:menu_id", controllerMenuItems.deleteMenu);
+    router.get("/api/admin/menu/:menu_id/menu-item", controllerMenuItems.getAllMenuItemsByMenuId);
+
     //MenuItems
-   /*  router.get("/api/admin/menu-item", controller.getAllMenuItems);
-    router.get("/api/admin/menu-item/:menu_item_id", controller.getMenuItemById);
-    router.post("/api/admin/menu-item", controller.createMenuItem);
-    router.put("/api/admin/menu-item/:menu_item_id",controller.editMenuItem);
-    router.delete("/api/admin/menu-item/:menu_item_id", controller.deleteMenuItem); */
+    router.get("/api/admin/menu-item", controllerMenuItems.getAllMenuItems);
+    router.get("/api/admin/menu-item/:menu_item_id", controllerMenuItems.getMenuItemById);
+    router.post("/api/admin/menu-item", controllerMenuItems.createMenuItem);
+    router.put("/api/admin/menu-item/:menu_item_id",controllerMenuItems.editMenuItem);
+    router.delete("/api/admin/menu-item/:menu_item_id", controllerMenuItems.deleteMenuItem);
+
     //Table
-    /* router.get("/api/admin/table", controller.getAllTables);
-    router.get("/api/admin/table/:table_id",controller.getTableById);
-    router.post("/api/admin/table", controller.createTable);
-    router.put("/api/admin/:table_id", controller.editTable);
-    router.delete("/api/admin/:table_id", controller.deleteTable); */
+    router.get("/api/admin/table", controllerTables.getAllTables);
+    router.get("/api/admin/table/:table_id",controllerTables.getTableById);
+    router.post("/api/admin/table", controllerTables.createTable);
+    router.put("/api/admin/:table_id", controllerTables.editTable);
+    router.delete("/api/admin/:table_id", controllerTables.deleteTable);
+
     //User
     router.get("/api/admin/user", controller.getAllUsers);
     router.get("/api/admin/user/:user_id", controller.getUserById);
     router.post("/api/admin/user", controller.createUser);
     router.put("/api/admin/user/:user_id", controller.editUser);
     router.delete("/api/admin/user/:user_id", controller.deleteUser);
+
     //Order
     /* router.get("/api/admin/orders", controller.getAllOrders);
     router.put("/api/admin/order/:order_id", controller.editOrder);
+
     //OrderFeedback
     router.get("/api/admin/feedback", controller.getAllFeedback); */
 
 
-    //Staff
+   // <------------------------STAFF------------------------------->
+
     //Tables
     /* router.get("/api/staff/table", controller.getAllTables);
     //Menu
