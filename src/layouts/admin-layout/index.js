e21047951c8d71bae94b544d98dabcb743c50657
Tejaswini268 <malothu.t@northeastern.edu>
2023-04-15 01:55:28 -0400

    resolving conflicts

--- src/layouts/admin-layout/index.js
+++ src/layouts/admin-layout/index.js
@@ -16,19 +16,23 @@ const AdminLayout = () => {
             <div class="logo">
             <img src={images.dineordel} alt="food img" />
             </div>
+
             <br /> <br /> <br />
             <div class="nav-item">
               <MaterialSymbol icon="Home" size={22} color="#8CBCF5" />  
               <Link to="/admin" className="link" color="$primary-light">Home</Link>
             </div>
+
             <div class="nav-item">
               <MaterialSymbol icon="Restaurant_menu" size={22} fill grade={-25} color="#8CBCF5" /> 
               <Link to="/admin/menu" className="link">Menu</Link>
             </div>
+
             <div class="nav-item">
               <MaterialSymbol icon="Table_bar" size={22} color="#8CBCF5"/> 
               <Link to="/admin/tables" className="link">Tables</Link>
             </div>
+
             <div class="nav-item">
               <MaterialSymbol icon="Group" size={22} color="#8CBCF5"/> 
               <Link to="/admin/staff" className="link">Staff</Link>
